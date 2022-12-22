14515f33e59cffe9f55b400791dcea71984ce022
Kacper <kacper.tylec1999@gmail.com>
2022-12-22 12:15:42 +0100

    initial auth schemas

--- schemas/authSchemas.ts
+++ schemas/authSchemas.ts
@@ -1,1 +1,13 @@
+import { z } from "zod";
 
+export const userRegisterSchema = z.object({
+  firstName: z.string().min(3).max(28),
+  lastName: z.string().min(3).max(28),
+  email: z.string().email().max(255),
+  password: z.string().min(8).max(200),
+});
+
+export const userLoginSchema = z.object({
+  email: z.string().email().max(255),
+  password: z.string().min(8).max(200),
+});
